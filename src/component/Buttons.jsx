91287df8e6d7d005decc2020cf46c543810514b9
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2022-07-19 12:45:19 +0300

    working

--- src/component/Buttons.jsx
+++ src/component/Buttons.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import AddIcon from '@mui/icons-material/Add';
 import Fab from '@material-ui/core/Fab';
 import Zoom from '@material-ui/core/Zoom';
 
@@ -7,21 +6,20 @@ function Buttons(props) {
 
     return (
         <div className={"create-note"}>
-            <Zoom in={props.isExpanded}>
+            <Zoom in={props.isExpanded}
+                  style={{background: '#FFB562'}}>
                 <Fab onClick={props.submitNote}>
-                    <AddIcon style={{background: '#FFB562'}}/>
                 </Fab>
             </Zoom>
             <Zoom in={props.isExpanded}
                   style={{transitionDelay: '250ms'}}>
-                <Fab onClick={props.submitNote} style={{ background: '#3AB0FF'}}>
-                    {/*<AddIcon/>*/}
+                <Fab onClick={props.submitNote}
+                     style={{ background: '#3AB0FF'}}>
                 </Fab>
             </Zoom>
             <Zoom in={props.isExpanded}
                   style={{transitionDelay: '500ms', background: '#F87474'}}>
                 <Fab onClick={props.submitNote}>
-                    {/*<AddIcon/>*/}
                 </Fab>
             </Zoom>
         </div>
