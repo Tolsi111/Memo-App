d9b816dc60038f9f9f7ab9924ecc9b7beceb5c9a
AndreiBacila <andrei.bacila@rodeapps.com>
2022-08-01 17:58:17 +0300

    Small fixes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import Fab from '@material-ui/core/Fab';
 
 function Note(props) {
 
-    function handleClick() {
+    function handleDelete() {
         props.onDelete(props.id);
+    }
+
+    function handleEdit() {
+        props.onEdit(props.id);
     }
 
     return (
         <div className={"note"}>
             <h1>{props.title}</h1>
             <p>{props.content}</p>
-            <Fab onClick={handleClick}>
+            <Fab onClick={handleDelete}>
                 <DeleteIcon style={{color: props.color}}/>
+            </Fab>
+            <Fab onClick={handleEdit}>
+                <EditIcon style={{color: props.color}}/>
             </Fab>
         </div>
     );
