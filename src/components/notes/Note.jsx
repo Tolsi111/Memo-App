00ab8b5f0a0342651946020c8b93bdb5b603b99f
AndreiBacila <andrei.bacila@rodeapps.com>
2022-08-04 12:02:06 +0300

    baby steps towards edit feature

--- src/components/notes/Note.jsx
+++ src/components/notes/Note.jsx
@@ -1,30 +1,70 @@
-import React from "react";
+import React, {useState, useContext, useEffect} from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import DoneIcon from '@mui/icons-material/Done';
 import Fab from '@material-ui/core/Fab';
-import {useContext} from "react";
 import AuthContext from "../../store/auth-context";
+import Buttons from "./Buttons";
 
 function Note(props) {
     const authCtx = useContext(AuthContext);
+
+    const [editMode, setEditMode] = useState(false);
+    const [editTitleFocus, setEditTitleFocus] = useState(false);
+    const [editContentFocus, setEditContentFocus] = useState(false);
+    const [editTitle, setEditTitle] = useState('');
+    const [editContent, setEditContent] = useState('');
+
+    useEffect(() => {
+        setEditMode(editTitleFocus || editContentFocus)
+    }, [editTitleFocus,editContentFocus])
+
 
     function handleDelete() {
         props.onDelete(props.id);
     }
 
     function handleEdit() {
-        props.onEdit(props.id);
+        console.log("Edit note....")
+        setEditMode(true);
+        //props.onEdit(props.id);
+    }
+
+    function handleDone() {
+        console.log("Finished editing note....")
+        console.log("New note title:" + editTitle)
+        console.log("New note content:" + editContent)
+        setEditMode(false);
     }
 
     return (
         <div className={"note"}>
-            <h1>{props.title}</h1>
-            <p>{props.content}</p>
+            {!editMode && <>
+                <h1>{props.title}</h1>
+                <p>{props.content}</p>
+            </>}
+            {editMode && <div className={"edit-note"}>
+                <input name={"title"}
+                       defaultValue={props.title}
+                       //onBlur={() => {setEditTitleFocus(false)}}
+                       //onFocus={() => {setEditTitleFocus(true)}}
+                       onChange={(e) => setEditTitle(e.target.value)}
+                />
+                <textarea name={"content"}
+                          defaultValue={props.content}
+                          //onBlur={() => {setEditContentFocus(false)}}
+                          //onFocus={() => {setEditContentFocus(true)}}
+                          onChange={(e) => setEditContent(e.target.value)}
+                />
+            </div>}
             {authCtx.isLoggedIn && props.ownerEmail === authCtx.email && <Fab onClick={handleDelete}>
                 <DeleteIcon style={{color: props.color}}/>
             </Fab>}
-            {authCtx.isLoggedIn && props.ownerEmail === authCtx.email && <Fab onClick={handleEdit}>
+            {!editMode && authCtx.isLoggedIn && props.ownerEmail === authCtx.email && <Fab onClick={handleEdit}>
                 <EditIcon style={{color: props.color}}/>
+            </Fab>}
+            {editMode && authCtx.isLoggedIn && props.ownerEmail === authCtx.email && <Fab onClick={handleDone}>
+                <DoneIcon style={{color: props.color}}/>
             </Fab>}
         </div>
     );
